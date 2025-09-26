a0f7485f8f7f53253a776001e1e095ee8458a63e
learnuidev <learnuidev@gmail.com>
2025-09-25 22:28:43 -0400

    wip

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   dts: true,
   format: "esm",
-  entry: ["src/adaptive/index.ts"],
+  entry: [
+    "src/adaptive/index.ts",
+
+    "./src/adaptive-provider.tsx", // bundle each component .ts or .tsx file
+  ],
 });
